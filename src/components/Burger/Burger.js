7ed9993e5e06765748586e048fc1b29034ee4672
roelver <roel@romaniflo.nl>
2017-12-24 00:15:48 +0100

    Reduce empty ingredients array to message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,12 +8,16 @@ const burger = (props) => {
             .map(igKey => {
                 return [...Array(props.ingredients[igKey])]  // Create an array of the correct length
                         .map((x, i) => <BurgerIngredient key={igKey + i} type={igKey} />)
-            });
+            })
+            .reduce((arr, el) => {
+                return arr.concat(el);
+            }, []);
     return (
         <div className={ classes.Burger }>
             <BurgerIngredient type="bread-top"/>
-            { ingredients }
-            {/* { ingredients.map(key => {
+            { ingredients.length > 0 ? ingredients : <p>Please add ingredients to your burger</p> }
+            {/* My solution:::::
+            { ingredients.map(key => {
                 const counter = props.ingredients[key];
                 let ingredientList = [];
                 for (let i = 0; i < counter; i++) {
