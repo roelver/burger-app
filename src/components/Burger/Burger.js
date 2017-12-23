5cf6173c7892717da98841c80b730722ee6d98f4
roelver <roel@romaniflo.nl>
2017-12-24 00:02:55 +0100

    Dynamic ingredients on Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const ingredients = Object.keys(props.ingredients)
+            .map(igKey => {
+                return [...Array(props.ingredients[igKey])]  // Create an array of the correct length
+                        .map((x, i) => <BurgerIngredient key={igKey + i} type={igKey} />)
+            });
     return (
         <div className={ classes.Burger }>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="chease"/>
-            <BurgerIngredient type="meat"/>
+            { ingredients }
+            {/* { ingredients.map(key => {
+                const counter = props.ingredients[key];
+                let ingredientList = [];
+                for (let i = 0; i < counter; i++) {
+                    ingredientList.push(<BurgerIngredient key={key + i} type={key} />);
+                }
+                return ingredientList;
+            })} */}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 2,
+            bacon: 1,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Fragment>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Fragment>
         );
